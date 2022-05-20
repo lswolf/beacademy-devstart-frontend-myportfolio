36485e1a04a1606246b4bb6878eecb016b3423af
lswolf <=>
2022-05-20 15:36:16 -0300

    responsive almost there (needs ajustments)

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,5 @@
+
+//swipper
 const swiper = new Swiper('.mySwiper', {
     // Optional parameters
     loop: true,
@@ -24,4 +26,24 @@ const swiper = new Swiper('.mySwiper', {
          },
     },
   });
-  
+  // show navigation
+  const nav = document.querySelector("header .container nav");
+
+const toggle = document.querySelectorAll(".toggle");
+
+for (element of toggle) {
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+}
+
+const links = document.querySelectorAll(
+  "nav .menu li "
+);
+//.menu-item, nav .btn-primary, nav .menu-social li a
+
+for (const link of links) {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
+}
